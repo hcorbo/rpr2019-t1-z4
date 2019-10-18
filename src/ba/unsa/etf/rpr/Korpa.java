5b09fdc8a2a3501e5f68ceec1830730ebe0cb399
Harun Corbo <hcorbo2@etf.unsa.ba>
2019-10-18 10:54:26 +0200

    Završena klasa Artikl sa geterima

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,5 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] niz = new Artikl[50];
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,7 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    Artikl[] artikli = new Artikl[1000];
+    public void dodajArtikl(Artikl biciklo) {
+    }
 }
