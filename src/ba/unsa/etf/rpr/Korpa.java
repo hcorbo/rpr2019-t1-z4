ce287488ae0e6bfdd35dde326cf9b2c7974bcbfb
Harun Corbo <hcorbo2@etf.unsa.ba>
2019-10-20 21:42:35 +0200

    Završena funkcija dodajArtikl Korpe

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,5 +1,17 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
-    private Artikl[] niz = new Artikl[50];
+    private final int BROJEL=50;
+    private Artikl[] artiklikorpe = new Artikl[BROJEL];
+    public boolean dodajArtikl(Artikl a) {
+        for(int i = 0; i < BROJEL; i++) {
+            if(artiklikorpe[i]==null) artiklikorpe[i] = a;
+            return true;
+        }
+        return false;
+    }
+
+    public Artikl[] getArtikli() {
+        return artiklikorpe;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -3,5 +3,12 @@ package ba.unsa.etf.rpr;
 public class Supermarket {
     Artikl[] artikli = new Artikl[1000];
     public void dodajArtikl(Artikl biciklo) {
+        for(int i = 0; i < 1000; i++) {
+
+        }
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
     }
 }
