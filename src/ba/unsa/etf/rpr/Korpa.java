90814b18d8434968c1b718d8272df5e81dedfbd5
Harun Corbo <hcorbo2@etf.unsa.ba>
2019-10-21 07:38:58 +0200

    Program radi sve sto je potrebno

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,17 +1,41 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
-    private final int BROJEL=50;
+    private final int BROJEL = 50;
     private Artikl[] artiklikorpe = new Artikl[BROJEL];
     public boolean dodajArtikl(Artikl a) {
         for(int i = 0; i < BROJEL; i++) {
-            if(artiklikorpe[i]==null) artiklikorpe[i] = a;
-            return true;
+            if(artiklikorpe[i]==null) {
+                artiklikorpe[i] = a;
+                return true;
+            }
         }
         return false;
     }
 
     public Artikl[] getArtikli() {
         return artiklikorpe;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl povratna = null;
+        for (int i = 0; i < BROJEL; i++){
+            if(artiklikorpe[i]!=null)
+            if(kod.equals(artiklikorpe[i].getKod()) == true) {
+                povratna = artiklikorpe[i];
+                artiklikorpe[i] = null;
+                return povratna;
+            }
+        }
+        return povratna;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int suma = 0;
+        for(int i = 0; i < BROJEL; i++) {
+            if (artiklikorpe[i]!=null)
+            suma = suma + artiklikorpe[i].getCijena();
+        }
+        return suma;
     }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,14 +1,33 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
-    Artikl[] artikli = new Artikl[1000];
-    public void dodajArtikl(Artikl biciklo) {
-        for(int i = 0; i < 1000; i++) {
+    private final int MAXELEMENT = 1000;
+    Artikl[] artikli = new Artikl[MAXELEMENT];
 
+    public boolean dodajArtikl(Artikl a) {
+        for(int i = 0; i < MAXELEMENT; i++) {
+            if(artikli[i]==null) {
+                artikli[i] = a;
+                return true;
+            }
         }
+        return false;
     }
 
     public Artikl[] getArtikli() {
         return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl povratna = null;
+        for (int i = 0; i < MAXELEMENT; i++){
+            if(artikli[i]!=null)
+            if(kod.equals(artikli[i].getKod()) == true) {
+                povratna = artikli[i];
+                artikli[i] = null;
+                return povratna;
+            }
+        }
+        return povratna;
     }
 }
